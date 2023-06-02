4a75ca04089136ce574372297b1fd7d5b7005a8a
samb-mac <samb21272@gmail.com>
2023-06-02 15:37:20 +0330

    login and register route add!

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { RegisterUserDto } from './dtos/register-user.dto';
+import { LoginUserDto } from './dtos/login-user.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  @Post('/register')
+  register(@Body() body: RegisterUserDto) {
+    console.log(body);
+  }
+
+  @Post('/login')
+  login(@Body() body: LoginUserDto) {
+    console.log(body);
+  }
+}
